14d1c8a980d715ce5f03494ecf2008d238c83f51
zzhan127 <zzhan127@ucsc.edu>
2020-01-27 05:51:18 -0800

    action key added(with no actual effects)

--- assets/script/playerMovement.cs
+++ assets/script/playerMovement.cs
@@ -6,6 +6,8 @@ public class playerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float moveH, moveV;
+	public Sprite dragon;
+	public Sprite human;
     [SerializeField] private float moveSpeed = 2.0f;
     
     private void Start(){
@@ -15,6 +17,12 @@ public class playerMovement : MonoBehaviour
     private void Update(){
         moveH = Input.GetAxisRaw("Horizontal")* moveSpeed;
         moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+		if(Input.GetKey("space")){
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = dragon;
+		}else{
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = human;
+		}
+			
     }
 
     private void FixedUpdate(){
