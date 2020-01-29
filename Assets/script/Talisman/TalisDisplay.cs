e98cda13a59ed89608c1997ed645b5f12f4092b0
Hardwin Bui <buihardwin@yahoo.com>
2020-01-29 02:35:18 -0800

    drag and drop implemented

---
+++ Assets/script/Talisman/TalisDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalisDisplay : MonoBehaviour
+{
+    public GameObject display;
+    public float timer;
+
+    private float curTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("space") && curTime <= 0){
+			display.SetActive(true);
+            curTime = timer;
+		}
+
+        if(curTime > 0){
+            curTime -= Time.deltaTime;
+        }
+        else if(curTime <= 0 && display.activeSelf){
+            display.SetActive(false);
+        }
+    }
+}
---
+++ Assets/script/Talisman/TalisDrag.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TalisDrag : MonoBehaviour, IDragHandler, IEndDragHandler, IDropHandler
+{
+    public enum Elements { METAL, WOOD, WATER, FIRE, EARTH }
+    public Elements element;
+    public Transform talisman;
+
+    private Vector3 origin;
+    private bool setTalis;
+
+    public void OnDrag(PointerEventData eventData){
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData){
+        if(!setTalis){
+            transform.position = origin;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData){
+        RectTransform invPanel = talisman as RectTransform;
+        
+        if(RectTransformUtility.RectangleContainsScreenPoint(invPanel, Input.mousePosition)){
+            setTalis = true;
+            Debug.Log("Made talisman of type: " + element);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        origin = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
