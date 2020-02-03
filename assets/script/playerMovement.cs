d9d319ebc83cf5f0a005bd5dfc2f23dc640f0f53
zzhan127 <zzhan127@ucsc.edu>
2020-02-03 07:41:49 -0800

    arts aupdate

--- assets/script/playerMovement.cs
+++ assets/script/playerMovement.cs
@@ -6,6 +6,8 @@ public class playerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float moveH, moveV;
+	public Sprite normal;
+	public Sprite casting;
     [SerializeField] private float moveSpeed = 2.0f;
     
     private void Start(){
@@ -15,6 +17,11 @@ public class playerMovement : MonoBehaviour
     private void Update(){
         moveH = Input.GetAxisRaw("Horizontal")* moveSpeed;
         moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+		if(Input.GetKey("space")){
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = casting;
+		}else{
+			this.gameObject.GetComponent<SpriteRenderer>().sprite = normal;
+		}
     }
 
     private void FixedUpdate(){
